464855c1bb4023cb2006ea58da716a7f70ff432c
Seth <relaysons@gmail.com>
2017-01-19 18:05:47 -0800

    Fixed typo

--- src/DeadbandXboxController.cpp
+++ src/DeadbandXboxController.cpp
@@ -4,7 +4,7 @@ double DeadbandXboxController::GetX(enum frc::GenericHID::JoystickHand hand, dou
 	double value = XboxController::GetX(hand);
 
 	// Apply Deadband
-	if(abs(value) <= deadband) {
+	if(fabs(value) <= deadband) {
 		return 0;
 	} else {
 		return value;
@@ -15,7 +15,7 @@ double DeadbandXboxController::GetY(enum frc::GenericHID::JoystickHand hand, dou
 	double value = XboxController::GetY(hand);
 
 	// Apply Deadband
-	if(abs(value) <= deadband) {
+	if(fabs(value) <= deadband) {
 		return 0;
 	} else {
 		return value;
